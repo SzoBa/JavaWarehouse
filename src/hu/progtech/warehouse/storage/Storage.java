1a07796ae5f249aec7301c344be20576d8b27ba2
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-06 16:35:04 +0200

    Add skeleton code

--- src/hu/progtech/warehouse/storage/Storage.java
+++ src/hu/progtech/warehouse/storage/Storage.java
@@ -7,10 +7,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Storage {
-    final int storageId;
-    String storageName;
-    List<StockItem> commodities = new LinkedList<>();
-    List<ProductType> allowedCommodities = new ArrayList<>();
+    private final int storageId;
+    private String storageName;
+    private final List<StockItem> commodities = new LinkedList<>();
+    private final List<ProductType> allowedCommodities = new ArrayList<>();
 
     public Storage(int storageId) {
         this.storageId = storageId;
@@ -19,4 +19,25 @@ public abstract class Storage {
     public abstract void addCommodity(StockItem item);
     public abstract StockItem getCommodity(int stockItemId);
     public abstract void deleteCommodity(int stockItemId);
+
+
+    public int getStorageId() {
+        return storageId;
+    }
+
+    public String getStorageName() {
+        return storageName;
+    }
+
+    public List<StockItem> getCommodities() {
+        return commodities;
+    }
+
+    public List<ProductType> getAllowedCommodities() {
+        return allowedCommodities;
+    }
+
+    public void setStorageName(String storageName) {
+        this.storageName = storageName;
+    }
 }
