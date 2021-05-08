8449542b7d7f632a1d04209d07fa3655aa7ae4df
Balázs Szolcsánszki <bszolcsan@gmail.com>
2021-05-08 13:16:29 +0200

    Change ProductItemProvider return type to fix

--- src/hu/progtech/warehouse/factory/ProductItemFactory.java
+++ src/hu/progtech/warehouse/factory/ProductItemFactory.java
@@ -10,8 +10,8 @@ import hu.progtech.warehouse.product.Product;
  * It declares the creation of items, requesting an item type and a Product class instance as arguments.
  * The condition, for creating Item instances used in the program, is to provide a Product instance (clone).
  */
-public interface ProductItemFactory<T> {
+public interface ProductItemFactory {
 
-    T createItem(ItemType item, Product product);
+    Item createItem(ItemType item, Product product);
 
 }
